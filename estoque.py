b609ac61a6e5b347075ff62b52bb5bc03c23130a
Tiago Mariano <tiago.mariano00@gmail.com>
2025-02-03 16:27:11 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -52,7 +52,6 @@ st.data_editor(
         "Faturado Mês Qt.": st.column_config.ProgressColumn(
             "Progresso de faturamento",
             help="Progresso em quantidade",
-            format=":.0f",
             min_value=0,
             
         ),
