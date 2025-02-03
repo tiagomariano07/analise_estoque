7e7653e4b348f0df913aeb2ecd102be447e399f0
Tiago Mariano <tiago.mariano00@gmail.com>
2025-02-03 17:11:41 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -95,4 +95,4 @@ if botao:
     st.write("Principais Itens - Tubos", df_tubos)
     st.write("Principais Itens - Conexões", df_conexoes_injetadas)
 
-st.dataframe(df_filtered)
+st.dataframe(df_filtered, df_filtered["Faturado Mês Qt."])
